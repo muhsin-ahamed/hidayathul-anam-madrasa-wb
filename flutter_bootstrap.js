4d11ec4164c792de12707b0c8ef2756b9066afe0
muhsin-ahamed <muhsinahamed411@gmail.com>
2026-08-10 23:47:21 +0530

    Update Flutter web build for Render API backend

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4008437195" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1260269471" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
